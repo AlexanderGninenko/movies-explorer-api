dff2e030fe2b21751421460c50fc2fd8d185b7bc
AlexanderGninenko <alexander.gninenko@icloud.com>
2023-05-19 16:27:18 +1000

    sameSite commented

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ const login = (req, res, next) => {
         .cookie("jwt", token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: "none",
+          // sameSite: "none",
           // secure: true,
         })
         .send({ token });
